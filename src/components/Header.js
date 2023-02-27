2d602d976618456781eaae459f82c0c774e2c791
Ben Stephens <65748213+BenStephens99@users.noreply.github.com>
2023-02-27 15:54:27 +0000

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
-import React from "react";  
+import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
-window.onscroll = function() {addShadow()}
+window.onscroll = function () { addShadow() }
 
-function addShadow (){
-    if(document.documentElement.scrollTop > 10) {
+function addShadow() {
+    if (document.documentElement.scrollTop > 10) {
         document.getElementById("header").classList.add('header-scrolled');
     } else {
         document.getElementById("header").classList.remove('header-scrolled');
@@ -14,24 +14,24 @@ function addShadow (){
 
 
 
-function Header () {
+function Header() {
     const [currentSelected, setCurrentSelected] = useState(window.location.pathname)
 
-    function updateSelected () {
+    function updateSelected() {
         setCurrentSelected(window.location.pathname)
     }
     return (
         <header id="header">
-        <div className="header-content">
-            <h1>Francis English</h1>
-            <ul onClick={updateSelected}>
-                <Link to="/about"><li className={currentSelected === "/about" ? "selected" : ""}>About</li></Link>
-                <Link to="/"><li className={currentSelected === "/" ? "selected" : ""}>Porfolio</li></Link>
-                <Link to="/contact"><li className={currentSelected === "/contact" ? "selected" : ""}>Contact</li></Link>
-            </ul>
-        </div>
-    </header>
-    ) 
+            <div className="header-content">
+                <Link onClick={updateSelected} to="/"><h1>Francis English</h1></Link>
+                <ul onClick={updateSelected}>
+                    <Link to="/about"><li className={currentSelected === "/about" ? "selected" : ""}>About</li></Link>
+                    <Link to="/"><li className={currentSelected === "/" ? "selected" : ""}>Porfolio</li></Link>
+                    <Link to="/contact"><li className={currentSelected === "/contact" ? "selected" : ""}>Contact</li></Link>
+                </ul>
+            </div>
+        </header>
+    )
 }
 
 export default Header;
