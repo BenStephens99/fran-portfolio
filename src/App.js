22376b7aebf7473b7e948ca62c94ae8600e665c9
Ben Stephens <65748213+BenStephens99@users.noreply.github.com>
2023-04-08 13:11:46 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
         <Routes>
           <Route path="/" element={<Porfolio />} />
@@ -15,7 +15,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       <Footer />
-    </div>
+    </>
   );
 }
 
